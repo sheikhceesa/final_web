3f3b991c060e1bd05340d9ffbf6e6e97757678f0
sheikhceesa <sheikhc081@gmail.com>
2025-07-10 12:28:59 +0100

    Add VSCode settings and custom error templates

--- app.py
+++ app.py
@@ -112,19 +112,6 @@ def role_required(roles):
         return decorated_function
     return decorator
 
-# Error handlers
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-@app.errorhandler(403)
-def forbidden(e):
-    return render_template('403.html'), 403
-
 # Routes
 @app.route('/')
 def home():
